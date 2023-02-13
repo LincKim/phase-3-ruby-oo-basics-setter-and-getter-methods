a884095dc1c8b4ee390c69ed0af49fa00b9be2cd
LincKim <lincolnkimutai@gmail.com>
2023-02-13 16:06:01 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
 
+    def name=(name)  #setter method
+        @name = name
+    end
+
+    def name # getter method
+        @name
+    end
+
+    def breed=(name)
+        @breed = name
+    end
+    
+    def breed 
+        @breed
+    end
+
 end
